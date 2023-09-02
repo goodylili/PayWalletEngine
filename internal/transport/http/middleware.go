d29436736cf951690d61cb4f899007f271f7d0d5
Goodnessuc <ukejegoodness599@gmail.com>
2023-09-02 10:39:39 +0100

    added middleware functionality

--- internal/transport/http/middleware.go
+++ internal/transport/http/middleware.go
@@ -1,1 +1,38 @@
 package http
+
+import (
+	"context"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"time"
+)
+
+// JSONMiddleware sets the request output to use JSON as the content type
+func JSONMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		next.ServeHTTP(w, r)
+	})
+}
+
+// TimeoutMiddleware sets the request to timeout after 15 seconds of initiation
+func TimeoutMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(r.Context(), 15*time.Second)
+		defer cancel()
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// LoggingMiddleware - a handy middleware function that logs out incoming requests
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.WithFields(
+			log.Fields{
+				"Method": r.Method,
+				"Path":   r.URL.Path,
+			}).
+			Info("handled request")
+		next.ServeHTTP(w, r)
+	})
+}
